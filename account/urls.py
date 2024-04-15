d339b0461ff4d3ab8e608e1200ef9a2a1d89ff9c
Shukurov Sardor <sardorshukurov2003S@gmail.com>
2024-04-15 23:13:58 +0500

    full

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include, re_path
 from .views import ChangePasswordView, RegisterView, MyObtainTokenPairView, LogoutAPIView, ForgotPasswordView, \
-    VerifyCodeView,UserUpdatenameView,AdminUpdatenameView
+    VerifyCodeView, UserUpdatenameView, AdminUpdatenameView, AdminLoginView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -13,11 +13,12 @@ urlpatterns = [
         path('logout/', LogoutAPIView.as_view(), name="logout"),
         re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
         # change
-        path('change_password/<int:id>', ChangePasswordView.as_view(), name='auth_change_password'),
+        path('change_password/', ChangePasswordView.as_view(), name='auth_change_password'),
         path('send_gmail_code/', ForgotPasswordView.as_view(), name='forgot_password'),
         path('code_check/', VerifyCodeView.as_view(), name='verification_code'),
         path('user_update_name/',UserUpdatenameView.as_view(), name='user_update_name'),
-        path('admin_update_name/',AdminUpdatenameView.as_view(), name='admin_update_name')
+        path('admin_update_name/',AdminUpdatenameView.as_view(), name='admin_update_name'),
+        path('admin_login/', AdminLoginView.as_view(), name='admin_login')
     ])),
 
 ]
